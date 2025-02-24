a1f2005b37e1662d2a0a0cee03e802c9d463a6e4
joetsui1994 <joetsui1994@gmail.com>
2025-02-24 16:08:31 +0000

    updated settings.prod

--- sophi_backend/settings/prod.py
+++ sophi_backend/settings/prod.py
@@ -1,7 +1,7 @@
 from .base import *
 
 DEBUG = False
-ALLOWED_HOSTS = ["139.59.166.206"]
+ALLOWED_HOSTS = ["139.59.166.206", 'localhost', '127.0.0.1']
 
 # Security settings
 SECURE_SSL_REDIRECT = True
