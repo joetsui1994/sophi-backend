39d6dff7672227d435b7a6a1fd2b37ede7de05a5
joetsui1994 <joetsui1994@gmail.com>
2025-03-18 23:24:51 +0000

    added new urls to simulations

--- simulations/urls.py
+++ simulations/urls.py
@@ -7,4 +7,8 @@ router = routers.DefaultRouter()
 urlpatterns = [
     path('', include(router.urls)),
     path('simulation-repository/', views.SimulationRepository.as_view()),
+    path('get-simulation-data/<slug:uuid>/', views.get_simulation_data),
+    path('get-inference-tree/<slug:uuid>/', views.get_inference_tree),
+    path('get-migratory-event-counts/<slug:uuid>/', views.get_migratory_event_counts),
+    path('get-earliest-introductions/<slug:uuid>/', views.get_earliest_introductions),
 ]
